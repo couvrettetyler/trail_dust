90bef03e09a02f06d1ce6025ddc8185178c2a268
Tyler Couvrette <couvrettetyler@gmail.com>
2023-09-24 15:37:45 -0600

    Next round of updates

--- javascript/index.js
+++ javascript/index.js
@@ -3,7 +3,11 @@ var images = [];
 var time = 3000;
 
 images[0] = './images/2023_group.jpg';
-images[1] = './images/2020_group.jpg';
+images[1] = './images/2022_group.jpg';
+images[2] = './images/2021_group.png';
+images[3] = './images/2020_group.jpg';
+images[4] = './images/2019_group.jpg';
+images[5] = './images/2017_group.jpg';
 
 function changeImg() {
 	document.group_pic.src = images[i]
